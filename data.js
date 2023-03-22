7cca5c3b27289b770117dd4e4e4bac4fefd9154a
Mariya Kleshchenko <96040805+mariegorova@users.noreply.github.com>
2023-03-22 19:41:57 +0300

    feat: add data.js

---
+++ data.js
@@ -0,0 +1,161 @@
+[
+ {
+    "id": 1,
+     "title": "Calls are not being received",
+      "date": "10.12.2013",
+      "contractor": "William Jensen",
+      "priority": 1,
+      "status": "In the queue"
+    },
+    {
+      "id": 2,
+      "title": "Problem with the Internet",
+      "date": "10.12.2013",
+      "contractor": "Charles Gardner",
+      "priority": 2,
+      "status": "In the work"
+    },
+    {
+      "id": 3,
+      "title": "The alarm system in the house does not work",
+      "date": "10.12.2013",
+      "contractor": "Kevin Hayes",
+      "priority": 3,
+      "status": "Done"
+    },
+    {
+      "id": 4,
+      "title": "Calls are not being received",
+      "date": "10.12.2013",
+      "contractor": "Nicholas Keller",
+      "priority": 1,
+      "status": "Rejected"
+    },
+    {
+      "id": 5,
+      "title": "Problem with the Internet",
+      "date": "10.12.2013",
+      "contractor": "Jason Burns",
+      "priority": 1,
+      "status": "In the work"
+    },
+    {
+      "id": 6,
+      "title": "Calls are not being received",
+      "date": "10.12.2013",
+      "contractor": "John Wright",
+      "priority": 2,
+      "status": "In the queue"
+    },
+    {
+      "id": 7,
+      "title": "No network",
+      "date": "10.12.2013",
+      "contractor": "Mark Lane",
+      "priority": 5,
+      "status": "Done"
+    },
+    {
+      "id": 8,
+      "title": "Problem with the Internet",
+      "date": "10.12.2013",
+      "contractor": "Louis Byrd",
+      "priority": 2,
+      "status": "Rejected"
+    },
+    {
+      "id": 9,
+      "title": "Calls are not being received",
+      "date": "10.12.2013",
+      "contractor": "Karen Blake",
+      "priority": 3,
+      "status": "Done"
+    },
+    {
+      "id": 10,
+      "title": "Problem with the Internet",
+      "date": "10.12.2013",
+      "contractor": "Frank Mitchell",
+      "priority": 4,
+      "status": "Rejected"
+    },
+    {
+      "id": 11,
+      "title": "Calls are not being received",
+      "date": "10.12.2013",
+      "contractor": "Donald Willis",
+      "priority": 1,
+      "status": "In the work"
+    },
+    {
+      "id": 12,
+      "title": "No network",
+      "date": "10.12.2013",
+      "contractor": "Rachel Ferguson",
+      "priority": 2,
+      "status": "In the queue"
+    },
+    {
+      "id": 13,
+      "title": "Problem with the Internet",
+      "date": "10.12.2013",
+      "contractor": "Ruth Kelly",
+      "priority": 2,
+      "status": "Done"
+    }, {
+        "id": 14,
+        "title": " " ,
+        "date": " ",
+        "contractor": " ",
+        "priority": " ",
+        "status": " "
+    }, 
+    {
+    "id": 15,
+    "title": "No network",
+    "date": "10.12.2013",
+    "contractor": "Brandon Holmes",
+    "priority": 4,
+    "status": "In the queue"
+    },
+    {
+    "id": 16,
+    "title": "No network",
+    "date": "10.12.2013",
+    "contractor": "Mary Atkins",
+    "priority": 4,
+    "status": "In the queue"
+    },
+    {
+    "id": 17,
+    "title": "Problem with the Internet",
+    "date": "10.12.2013",
+    "contractor": "Melissa Norman",
+    "priority": 2,
+    "status": "Rejected"
+    },
+    {
+    "id": 18,
+    "title": "Calls are not being received",
+    "date": "10.12.2013",
+    "contractor": "Frank Mitchell",
+    "priority": 4,
+    "status": "Done"
+    },
+    {
+    "id": 19,
+    "title": "Problem with the Internet",
+    "date": "10.12.2013",
+    "contractor": "Christopher Davis",
+    "priority": 5,
+    "status": "In the work"
+    },
+    {
+    "id": 20,
+    "title": "No network",
+    "date": "10.12.2013",
+    "contractor": "Pedro Stewart",
+    "priority": 5,
+    "status": "In the work"
+    } ]
+    
